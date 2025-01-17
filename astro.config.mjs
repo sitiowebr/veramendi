59d43c1f4abfb3672e8c986a670f53b14ac93018
dxnsitio-web <rubenveramendi154@gmail.com>
2025-01-17 17:52:21 -0500

    cambios nuevos dxn

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://sitiowebr.github.io/veramendi',
-  base: '/veramendi',
+  site: 'https://sitiowebr.github.io/veramendi.dxn',
+  base: '/veramendi.dxn',
 });
